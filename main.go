380786000b4e32fa4fa651da971c0f3e2f50fb3e
Juris Gribuska <juris.gribuska@itv.com>
2023-08-06 05:28:01 +0100

    main complete

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
+package main
 
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+
+	"my-terraform-provider/random_joke"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return random_joke.Provider()
+		},
+	})
+}
